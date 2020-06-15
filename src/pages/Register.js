576ec8525562a03ec199b5f526e58d3e0009a9f7
PengMeng <383867421@qq.com>
2020-06-15 18:16:34 +0800

    注册页面  表单验证完善

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -33,6 +33,18 @@ const Register = () => {
     console.log('Failed:', errorInfo);
   };
 
+  const validateUsername = (rule, value) => {
+    if(/\W/.test(value)) {return Promise.reject('只能是字母数字下划线');}
+    if(value.length < 4 || value.length > 10) return Promise.reject('长度为4～10个字符');
+    return Promise.resolve();
+  };
+
+const  validateConfirm=({getFieldValue})=>({
+  validator(rule, value) {
+    if(getFieldValue('password') === value) return Promise.resolve();
+    return Promise.reject('两次密码不一致');
+  }
+})
   return (
     <Wraper>
       <Title>注册</Title>
@@ -46,7 +58,11 @@ const Register = () => {
       <Form.Item
         label="用户名"
         name="username"
-        rules={[{ required: true, message: '请输入用户名' }]}
+        rules={[{ required: true, message: '请输入用户名' },
+          {validator:validateUsername},
+
+
+          ]}
       >
         <Input />
       </Form.Item>
@@ -54,14 +70,20 @@ const Register = () => {
       <Form.Item
         label="密码"
         name="password"
-        rules={[{ required: true, message: '请输入密码' }]}
+        rules={[{ required: true, message: '请输入密码' },
+          {min:4,message: '最小4位密码'},
+          {max:10,message: '最大10位密码'}
+
+          ]}
       >
         <Input.Password />
       </Form.Item>
       <Form.Item
         label="确认密码"
-        name="password"
-        rules={[{ required: true, message: '请确认密码' }]}
+        name="confirmPassword"
+        rules={[{ required: true, message: '请确认密码' },
+          validateConfirm
+        ]}
       >
         <Input.Password />
       </Form.Item>
