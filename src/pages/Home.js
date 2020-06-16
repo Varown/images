6df107be54864531ce32b240cc4dc740704d694b
PengMeng <383867421@qq.com>
2020-06-16 10:39:23 +0800

    优化Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { observer } from 'mobx-react';
 import Uploader from "../components/Uploader";
 import Tips from "../components/Tips";
 
 
-const Home=observer(()=>{
+const Home=()=>{
 
   return(
     <>
@@ -15,5 +14,5 @@ const Home=observer(()=>{
     </>
   )
 
-})
+}
 export default Home
