bf4aec7a09cbd91c17693ac2a5615bfcbe432879
PengMeng <383867421@qq.com>
2020-06-16 10:04:20 +0800

    优化未登录展示

--- src/components/Tips.js
+++ src/components/Tips.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {useStores} from "../stores";
 import { observer } from 'mobx-react';
+import styled from 'styled-components';
+
+const StyledTips = styled.div`
+  background: orange;
+  padding: 10px;
+  margin: 30px 0;
+  color: #fff;
+  border-radius: 4px;
+`;
 const Tips=observer(({children})=>{
   const {UserStore}=useStores()
   return(
-    <>
+    <StyledTips>
       {
         UserStore.currentUser?null:<div> {children}</div>
       }
-    </>
+    </StyledTips>
   )
 })
 export default Tips
