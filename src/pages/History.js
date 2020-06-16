8c11f839e452dad9f25f55b4e1c741b662920963
PengMeng <383867421@qq.com>
2020-06-16 17:32:14 +0800

    连接

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,13 +1,15 @@
 import React from "react";
 import List from "../components/List";
+
 function History() {
   return(
     <>
       <h1>History</h1>
-      <List/>
+      <List>
+
+      </List>
       </>
   )
 
 }
-
 export default History
